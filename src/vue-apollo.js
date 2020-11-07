0461afa7f77929c1ebef8d6eca05f9dce8afb2e1
Wizston - Olawale Adegboyega <wizston@gmail.com>
2020-11-07 20:08:00 +0100

    Heroku final update

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -17,7 +17,7 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://https://pangaea-interviews.now.sh/api/graphql',
+  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'wss://https://pangaea-interviews.now.sh/api/graphql',
   // LocalStorage token
   tokenName: AUTH_TOKEN,
   // Enable Automatic Query persisting with Apollo Engine
